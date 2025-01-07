f5fc0e286710f5d42362b28434befeba022e7827
LudoKing-Unofficial <simplehacker02@gmail.com>
2025-01-07 19:13:30 +0530

    Add files via upload

--- routes/roomtype.js
+++ routes/roomtype.js
@@ -12,37 +12,15 @@ router.get('/:roomcode',  async (req, res) => {
     const roomcode = req.params.roomcode; // Capture the input parameter
 
     try{
-        const result = await axios.get(`https://apihubs.in/api/api.php?roomcode=${roomcode}&roomtype=json&apikey=${apiKey}`);
+        const result = await axios.get(`https://apihubs.in/api/api?roomcode=${roomcode}&roomtype=roomtype&apikey=${apiKey}`);
         res.json({
             "error": "false",
             "code": "200",
             "message": "data found",
             "data" : result.data.data.data
-        }); 
-        
-    }catch(err){
-        // console.log(err)
-        res.json({
-            "error": "true",
-            "code": "500",
-            "message": "Data Not found",
-            "data" : ''
         });
-    }
-    
-});
 
-router.get('/advance/:roomcode',  async (req, res) => {
-    const roomcode = req.params.roomcode; // Capture the input parameter
-
-    try{
-        const result = await axios.get(`https://apihubs.in/api/api.php?roomcode=${roomcode}&roomtype=json2&apikey=${apiKey}`);
-        res.json({
-            "error": "false",
-            "code": "200",
-            "message": "data found",
-            "data" : result.data.data.data
-        }); 
+            
         
     }catch(err){
         // console.log(err)
